9480217337ba3740992b6bf2ca8f0740f54fae5b
Sam Lau <samlau95@gmail.com>
2014-11-11 03:30:37 -0800

    App directory file builders

--- lib/bluebase/app_builder.rb
+++ lib/bluebase/app_builder.rb
@@ -42,5 +42,34 @@ module Bluebase
       copy_file "Guardfile", "Guardfile"
     end
 
+    #########################################################
+    # app/ directory files
+    #########################################################
+    def add_vendor_dirs
+      %w(app/assets/javascripts/vendor app/assets/stylesheets/vendor).each do |dir|
+        run "mkdir #{dir}"
+        run "touch #{dir}/.keep"
+      end
+    end
+
+    def replace_application_css_with_scss
+      base_dir = "app/assets/stylesheets"
+      remove_file "#{base_dir}/application.css"
+      copy_file "app/application.css.scss", "#{base_dir}/application.css.scss"
+    end
+
+    def add_application_folder_and_files_to_views
+      base_dir = "app/views/application"
+      empty_directory base_dir
+      copy_file "app/_flash.html.slim", "#{base_dir}/_flash.html.slim"
+      copy_file "app/_ga_boiler.html.slim", "#{base_dir}/_ga_boiler.html.slim"
+      copy_file "app/_fb_meta_tags.html.slim", "#{base_dir}/_fb_meta_tags.html.slim"
+    end
+
+    def replace_application_erb_with_slim
+      base_dir = "app/views/layouts"
+      remove_file "#{base_dir}/application.html.erb"
+      copy_file "app/application.html.slim", "#{base_dir}/application.html.slim"
+    end
   end
 end
