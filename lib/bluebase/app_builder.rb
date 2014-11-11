2d4f7909729f57045798fdae1b60c4925d95907f
Sam Lau <samlau95@gmail.com>
2014-11-11 02:47:20 -0800

    Build root files

--- lib/bluebase/app_builder.rb
+++ lib/bluebase/app_builder.rb
@@ -1,4 +1,46 @@
 module Bluebase
   class AppBuilder < Rails::AppBuilder
+    include Bluebase::Actions
+
+    #########################################################
+    # Root directory files
+    #########################################################
+    def readme
+      template "README.md.erb", "README.md"
+    end
+
+    def replace_gemfile
+      remove "Gemfile"
+      template "Gemfile.erb", "Gemfile"
+    end
+
+    def add_envrc
+      copy_file ".envrc", ".envrc"
+    end
+
+    def replace_gitignore
+      remove_file ".gitignore"
+      copy_file "gitignore", ".gitignore"
+    end
+
+    def add_rubocop_and_hound_config
+      copy_file ".hound.yml", ".hound.yml"
+      copy_file ".rubocop.yml", ".rubocop.yml"
+    end
+
+    def add_rvm_config
+      create_file ".ruby-gemset",  "#{app_path}\n"
+      create_file ".ruby-version", "#{Bluebase::RUBY_VERSION}\n"
+    end
+
+    def add_travis_config
+      copy_file ".travis.yml", ".travis.yml"
+    end
+
+    def add_guardfile
+      # TODO: Guardfile needs more finetuning
+      copy_file "Guardfile", "Guardfile"
+    end
+
   end
 end
