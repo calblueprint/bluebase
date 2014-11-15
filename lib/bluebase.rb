b8b18b0307725c6a9e6164ea75b1f3874ccb343f
Sam Lau <samlau95@gmail.com>
2014-11-15 01:13:07 -0800

    Remove stub method

--- lib/bluebase.rb
+++ lib/bluebase.rb
@@ -2,9 +2,3 @@ require "bluebase/version"
 require "bluebase/generators/app_generator"
 require "bluebase/actions"
 require "bluebase/app_builder"
-
-module Bluebase
-  def self.hello
-    'world'
-  end
-end
