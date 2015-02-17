6529638102716d43b97b428153e70ac61f06e944
Sam Lau <samlau95@gmail.com>
2015-02-16 20:52:28 -0800

    Rails 4.2.0 and version 1.1.0

--- lib/bluebase/version.rb
+++ lib/bluebase/version.rb
@@ -1,5 +1,5 @@
 module Bluebase
-  RAILS_VERSION = "4.1.8"
+  RAILS_VERSION = "4.2.0"
   RUBY_VERSION = IO.read("#{File.dirname(__FILE__)}/../../.ruby-version").strip
-  VERSION = "1.0.1"
+  VERSION = "1.1.0"
 end
